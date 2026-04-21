71b31ead2823a04ec4f4eb01af781d94c16ce811
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-21 15:49:02 +0000

    vending

--- naming-variables.c
+++ naming-variables.c
@@ -2,6 +2,7 @@
 
 int main(void){
 int number; //there was a exclamation mark
-int test_value;
-int example;
+int test_value; //replace the space with a sign
+int example1; //change the number of place (it can't be on the first place)
+float int_variables; //write more letters
 }
