30159c6391cfc162620d4595e428e526c7558b25
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-22 15:40:07 +0000

    vending machine

--- vending.c
+++ vending.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    //Variables for Tortas del 8
-    float price1 = 74.99;
-    int quantity1 = 15;
-    char select1 = "t";
+    //Variables for Cacaosuyo
+    float price1 = 5.99;
+    int quantity1 = 5;
+    char select1 = 'c';
 
     //Variables for Takis
-    float price2 = 28.90;
-    int quantity2 = 10;
-    char select2 = 'p';
+    float price2 = 1.80;
+    int quantity2 = 3;
+    char select2 = 't';
+
+    //Variables for Sprite 500 ml
+    float price3 = 1.90;
+    int quantity3 = 2;
+    char select3 = 's';
 
     //Print stock of vending machine
-    pirntf("Vending Machine Stock\n");
-    printf("-------------------------------\n);
-    printf("Item name: Tortas del 8\n");
+    printf("Vending Machine Stock\n");
+    printf("-------------------------------\n");
+    printf("Item name: Cacaosuyo\n");
+
 }
