f5405f5a8ddf9cfcbea178c8e3bcad128a782057
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-22 15:39:21 +0000

    vending machine

---
+++ vending.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    //Variables for Tortas del 8
+    float price1 = 74.99;
+    int quantity1 = 15;
+    char select1 = "t";
+
+    //Variables for Takis
+    float price2 = 28.90;
+    int quantity2 = 10;
+    char select2 = 'p';
+
+    //Print stock of vending machine
+    pirntf("Vending Machine Stock\n");
+    printf("-------------------------------\n);
+    printf("Item name: Tortas del 8\n");
+}
