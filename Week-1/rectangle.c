97a3d23473bad9f6669a7610e433dd6ed7846f0d
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-27 16:39:05 +0000

    h|

--- Week-1/rectangle.c
+++ Week-1/rectangle.c
@@ -2,8 +2,11 @@
 
 int main(void){
 
-    char number[90];
-    printf("Enter the width for the triangle:");
-    scanf("%s", &number);
-    printf("Number = %s!\n",number);
+    int length;
+    int width;
+    printf("Enter the length for the triangle: \n");
+    scanf("%d", &length);
+    printf("Enter the width for the triangle: \n");
+    scanf("%d", &width);
+    int Area =
 }
