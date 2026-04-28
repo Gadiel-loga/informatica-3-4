c92633b5e3a0d7fd1ff579062d1c8620a20cb389
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-28 15:42:19 +0000

    temp

--- temperature.c
+++ temperature.c
@@ -5,7 +5,6 @@ int main(void)
 float temperature;
 printf("Enter the temperature for Fahrenheit: \n");
 scanf("%f", &temperature);
-
-
-
+float C = (temperature-32)/1.8;
+printf("temperature°F is equal to %f°C\n", C);
 }
