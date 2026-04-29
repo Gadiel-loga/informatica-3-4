a07dee9bc03c33218f74d266376d27d966a83753
Gadiel-loga <christiangadielloga@gmail.com>
2026-04-29 14:55:46 +0000

    Walimai

---
+++ test/receipt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int item;
+    printf("Item: \n");
+    scanf("%d", &item);
+    float price;
+    printf("Price: \n");
+    scanf("%f", &price);
+    int quantity;
+    printf("Quantity bought: \n");
+    scanf("%d", &quantity);
+    printf("\n");
+    printf("**********************************\n");
+    printf("Supermarket ta' Walimai\n");
+    printf("**********************************\n");
+    printf("Item\tQuantity\tPrice\n");
+
+
+}
