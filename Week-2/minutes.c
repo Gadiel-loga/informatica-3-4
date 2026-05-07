6a4486af41fd1aa3c923aba4cc4c2e4cc0040698
Gadiel-loga <christiangadielloga@gmail.com>
2026-05-07 14:55:53 +0000

    minutes

--- Week-2/minutes.c
+++ Week-2/minutes.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 int minutes;
-printf("Enter the run time of the movie in minutes:");
+printf("Movie running time:");
 scanf("%d", &minutes);
 
 int start_h;
@@ -12,11 +12,15 @@ printf("Start Time: ");
 scanf("%d:%d", &start_h, &start_m);
 
 int hours = minutes / 60;
-int min = minutes & 60;
+int min = minutes % 60;
 
-int end_h = start_h + hours;
-int end_m = start_m + min;
+int end_h = start_h + hours; //end_h = 8
+int end_m = start_m + min; //end_m = 61
 
+if (end_m >=60) {
+    end_h = end_h + (end_m / 60); // Update variable value
+    end_m = end_m % 60;
+}
 printf("The movie will last %d hr and %d min.\n", hours, min);
-printf("End time: %d:%d \n", end_h, end_m);
+printf("End time: %d:%02d \n", end_h, end_m);
 }
