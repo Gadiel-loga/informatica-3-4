c49e12a44f9631ed40e3f3d156cd7c91e14ec727
Gadiel-loga <christiangadielloga@gmail.com>
2026-05-08 21:05:56 +0000

    temperature

--- Week-2/temperature.c
+++ Week-2/temperature.c
@@ -7,4 +7,22 @@ printf("Enter the temperature for Fahrenheit: \n");
 scanf("%f", &temperature);
 float C = (temperature-32)/1.8;
 printf("%.2f°F is equal to %.2f°C\n",temperature, C);
+if (C <= 0) {
+    printf("%.2f is ❄️ Freezing weather.\n", C);
+}
+if (C >= 0) {
+    printf("%.2f is 🥶 Very cold weather.\n", C);
+}
+if (C >= 10) {
+    printf("%.2f is 🧥 Chilly weather.\n", C);
+}
+if (C >= 20) {
+    printf("%.2f is 🖼️ Normal weather.\n", C);
+}
+if (C >= 30) {
+    printf("%.2f is ☀️ Hot weather.\n", C);
+}
+if (C >= 40) {
+    printf("%.2f is 🔥 Very hot weather.\n", C);
+}
 }
